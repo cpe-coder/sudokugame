67cfee9cc2b891216c4211721fa71ea3785fd863
Grian Gajila <griangajila2@gmail.com>
2023-08-07 17:10:25 +0800

    wip

--- static/js/script.js
+++ static/js/script.js
@@ -6,9 +6,17 @@ document.querySelector('#dark-mode-toggle').addEventListener('click', () =>{
     document.querySelector('meta[name="theme-color"').setAttribute('content', isDark ? '22222c': '#fff');
 })
 
+const getGameInfo = () =>  JSON.parse(localStorage.getItem('game'));
+
+
 const init = () =>{
     const darkmode = JSON.parse(localStorage.getItem('darkmode'));
     document.body.classList.add(darkmode? 'dark': 'light');
     document.querySelector('meta[name="theme-color"').setAttribute('content', isDark ? '22222c': '#fff');
 
+    const game =  getGameInfo();    
+
+    document.querySelector('#btn-continue').style.display = game ? 'grid' : 'none'; 
 }
+
+init();
