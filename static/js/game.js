68a424b0bcd0d620aeae45d72bffa70393bb8e67
Grian Gajila <griangajila2@gmail.com>
2023-08-08 13:04:38 +0800

    wip

---
+++ static/js/game.js
@@ -0,0 +1,29 @@
+const newGrid = (size) => {
+    let arr = new Array(size);
+
+    for (let i = 0; i < size; i++) {
+        arr[i] = new Array(size);  
+    }
+
+    for (let i = 0; i < Math.pow(size, 2); i++) {
+        arr[Math.floor(i/size)][i%size] = CONSTANT.UNASSIGNED;
+    }
+
+    return arr;
+}
+
+// check duplicate number in col
+const isColSafe = (grid, col, value) => {
+    for (let row = 0; row < CONSTANT.GRID_SIZE; row++) {
+        if (grid[row][col] === value) return false;
+    }
+    return true;
+}
+
+// check duplicate number in row
+const isRowSafe = (grid, row, value) => {
+    for (let col = 0; col < CONSTANT.GRID_SIZE; col++) {
+        if (grid[row][col] === value) return false;
+    }
+    return true;
+}
--- static/js/script.js
+++ static/js/script.js
@@ -6,7 +6,32 @@ document.querySelector('#dark-mode-toggle').addEventListener('click', () =>{
     document.querySelector('meta[name="theme-color"').setAttribute('content', isDark ? '22222c': '#fff');
 })
 
+const start_screen = document.querySelector('#start-screen');
+const game_screen = document.querySelector('#game-screen');
+const pause_screen = document.querySelector('#pause-screen');
+const result_screen = document.querySelector('#result-screen')
+const cells = document.querySelector('#main-grid-cell');
+const name_input = document.querySelector('#input-name');
+const number_inputs = document.querySelector('.number');
+const player_name = document.querySelector('#player-name');
+const game_level = document.querySelector('#game-level');
+const game_time = document.querySelector('#result-time');
+
 const getGameInfo = () =>  JSON.parse(localStorage.getItem('game'));
+
+
+const initGameGrid = () =>{
+    let index = 0;
+
+    for (let i = 0; i < Math.pow(level.GRID_SIZE,2); i++){
+        let row = Math.floor(i/level.GRID_SIZE);
+        let col = i % level.GRID_SIZE;
+        if (row === 2 || row === 5) cells[index].style.marginBottom = '10px';
+        if (col === 2 || col === 5) cells[index].style.marginRight = '10px';
+
+        index++;
+    }
+}
 
 const init = () =>{
     const darkmode = JSON.parse(localStorage.getItem('darkmode'));
